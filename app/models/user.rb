f625571eb2aa8702c33460327f516cddb91cd82a
Dorcas Laryea <naa1995ad@gmail.com>
2022-02-27 23:50:52 +0000

    tests updated, all tests passing

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
     validates_format_of :email, with: /@/
     validates :password_digest, presence: true
     has_secure_password 
+    # check secure password
 end
 #next steps tomorrow
